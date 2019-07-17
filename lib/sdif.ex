1e53c4af397fe468a735b2dd35e8a367fe0fccb3
Chris Ortman <chris-ortman@uiowa.edu>
2019-07-17 18:30:25 -0500

    First commit

---
+++ lib/sdif.ex
@@ -0,0 +1,18 @@
+defmodule Sdif do
+  @moduledoc """
+  Documentation for Sdif.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sdif.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sdif_test.exs
@@ -0,0 +1,8 @@
+defmodule SdifTest do
+  use ExUnit.Case
+  doctest Sdif
+
+  test "greets the world" do
+    assert Sdif.hello() == :world
+  end
+end
