1c200651b22dc31e161e37f822a6809524993bd0
Chris Ortman <chrisortman@gmail.com>
2019-07-17 22:42:16 -0500

    Parsing progress

--- lib/sdif.ex
+++ lib/sdif.ex
@@ -15,4 +15,100 @@ defmodule Sdif do
   def hello do
     :world
   end
+
+  def parse(<<"A0", line::binary>>) do
+
+    <<
+      org::binary-size(1),
+      v::binary-size(8),
+      file_code::binary-size(2),
+      fu1 ::binary-size(30),
+      software_name::binary-size(20),
+      software_version::binary-size(10),
+      contact_name::binary-size(20),
+      contact_phone::binary-size(12),
+      file_date::binary-size(8),
+      _ :: binary>> = line
+
+    {:file_description, [
+      org: org,
+      sdif_version: String.trim_trailing(v),
+      file_code: file_code,
+      software_name: String.trim_trailing(software_name),
+      software_version: String.trim_trailing(software_version),
+      contact_name: String.trim_trailing(contact_name),
+      contact_phone: String.trim_trailing(contact_phone),
+      file_date: parse_date(file_date),
+      fu1: fu1
+    ]}
+  end
+
+  def parse(<<"B1", line::binary>>) do
+    <<
+      org::binary-size(1),
+      _ :: binary-size(8),
+      meet_name :: binary-size(30),
+      meet_add_1 :: binary-size(22),
+      meet_add_2 :: binary-size(22),
+      meet_city :: binary-size(20),
+      meet_state :: binary-size(2),
+      meet_zip :: binary-size(10),
+      country_code :: binary-size(3),
+      meet_code :: binary-size(1),
+      meet_start :: binary-size(8),
+      meet_end :: binary-size(8),
+      altitude :: binary-size(4),
+      _ :: binary-size(8),
+      course_code :: binary-size(1),
+      _ :: binary >> = line
+    {:meet_record, []}
+  end
+
+
+  def print({:file_description, items}) do
+    fields = [
+      :org,
+      {:sdif_version, 8},
+      :file_code,
+      {:fu1, 30},
+      {:software_name, 20},
+      {:software_version, 10},
+      {:contact_name, 20},
+      {:contact_phone, 12},
+      {:file_date, :date},
+      {:blank, 42},
+      {:blank, 2},
+      {:blank, 3}
+    ] |> Enum.map(fn
+
+      f when is_atom(f) -> Keyword.get(items,f)
+      {:blank, pad} -> String.pad_trailing("",pad)
+      {f, :date} -> 
+        d = Keyword.get(items,f)
+        month = Integer.to_string(d.month) |> String.pad_leading(2,"0")
+        day = Integer.to_string(d.day) |> String.pad_leading(2,"0")
+        year = Integer.to_string(d.year)
+        month <> day <> year
+      {f,pad} -> Keyword.get(items,f) |> String.pad_trailing(pad)
+
+    end) |> Enum.join
+
+    "A0" <> fields <> "\n\n"
+  end
+
+  defp parse_date(str) do
+
+    <<
+      month::binary-size(2),
+      day::binary-size(2),
+      year::binary-size(4)
+    >> = str
+
+    [year,month,day] =
+      [year,month,day]
+      |> Enum.map(&String.to_integer/1)
+
+    {:ok, d} = Date.new(year,month,day)
+    d
+  end
 end
--- test/sdif_test.exs
+++ test/sdif_test.exs
@@ -2,7 +2,15 @@ defmodule SdifTest do
   use ExUnit.Case
   doctest Sdif
 
-  test "greets the world" do
-    assert Sdif.hello() == :world
+  test "reads file description record" do
+    sample =
+"""
+A01V3      01Meet Entries                  TeamUnify, LLC      2.0       Coach, Some         3191112222  05282019
+
+"""
+    # assert Sdif.parse(sample) == {:file_description, [org: "1", sdif_version: "V3", file_code: "01", software_name: "TeamUnify, LLC", software_version: "2.0", contact_name: "Coach, Some", contact_phone: "3191112222", file_date: ~D[2019-05-28]]}
+
+    roundtrip =  Sdif.parse(sample) |> Sdif.print
+    assert roundtrip == sample
   end
 end
