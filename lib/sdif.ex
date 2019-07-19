a0ace161211e89c654b2865d4697393b9771cca0
Chris Ortman <chrisortman@gmail.com>
2019-07-18 19:30:20 -0500

    Rework parse & print

--- lib/sdif.ex
+++ lib/sdif.ex
@@ -18,29 +18,51 @@ defmodule Sdif do
 
   def parse(<<"A0", line::binary>>) do
 
-    <<
-      org::binary-size(1),
-      v::binary-size(8),
-      file_code::binary-size(2),
-      fu1 ::binary-size(30),
-      software_name::binary-size(20),
-      software_version::binary-size(10),
-      contact_name::binary-size(20),
-      contact_phone::binary-size(12),
-      file_date::binary-size(8),
-      _ :: binary>> = line
+    # <<
+    #   org::binary-size(1),
+    #   v::binary-size(8),
+    #   file_code::binary-size(2),
+    #   fu1 ::binary-size(30),
+    #   software_name::binary-size(20),
+    #   software_version::binary-size(10),
+    #   contact_name::binary-size(20),
+    #   contact_phone::binary-size(12),
+    #   file_date::binary-size(8),
+    #   _ :: binary>> = line
 
-    {:file_description, [
-      org: org,
-      sdif_version: String.trim_trailing(v),
-      file_code: file_code,
-      software_name: String.trim_trailing(software_name),
-      software_version: String.trim_trailing(software_version),
-      contact_name: String.trim_trailing(contact_name),
-      contact_phone: String.trim_trailing(contact_phone),
-      file_date: parse_date(file_date),
-      fu1: fu1
-    ]}
+    fields = field_list(:file_description)
+
+    {line, items} = Enum.reduce(fields, {line,[]}, fn f, {rest,parsed} ->
+      {f_key, f_type, f_length} = f
+      case rest do
+        <<f_val::binary-size(f_length),unparsed::binary>> ->
+
+          final_val = case f_type do
+            :date -> parse_date(f_val)
+            _ -> String.trim(f_val)
+          end
+          items = Keyword.put(parsed, f_key, final_val)
+          {unparsed, items}
+        _ ->
+          IO.puts "Failed match"
+          IO.inspect f
+          IO.inspect rest
+          IO.puts "-----------------\n"
+          {"", parsed}
+      end
+    end)
+    {:file_description, Enum.reverse(items)}
+    # {:file_description, [
+    #   org: org,
+    #   sdif_version: String.trim_trailing(v),
+    #   file_code: file_code,
+    #   software_name: String.trim_trailing(software_name),
+    #   software_version: String.trim_trailing(software_version),
+    #   contact_name: String.trim_trailing(contact_name),
+    #   contact_phone: String.trim_trailing(contact_phone),
+    #   file_date: parse_date(file_date),
+    #   fu1: fu1
+    # ]}
   end
 
   def parse(<<"B1", line::binary>>) do
@@ -64,38 +86,50 @@ defmodule Sdif do
     {:meet_record, []}
   end
 
-
-  def print({:file_description, items}) do
-    fields = [
-      :org,
-      {:sdif_version, 8},
-      :file_code,
-      {:fu1, 30},
-      {:software_name, 20},
-      {:software_version, 10},
-      {:contact_name, 20},
-      {:contact_phone, 12},
-      {:file_date, :date},
-      {:blank, 42},
-      {:blank, 2},
-      {:blank, 3}
-    ] |> Enum.map(fn
-
-      f when is_atom(f) -> Keyword.get(items,f)
-      {:blank, pad} -> String.pad_trailing("",pad)
-      {f, :date} -> 
-        d = Keyword.get(items,f)
-        month = Integer.to_string(d.month) |> String.pad_leading(2,"0")
-        day = Integer.to_string(d.day) |> String.pad_leading(2,"0")
-        year = Integer.to_string(d.year)
-        month <> day <> year
-      {f,pad} -> Keyword.get(items,f) |> String.pad_trailing(pad)
-
-    end) |> Enum.join
-
-    "A0" <> fields <> "\n\n"
+  def parse(other) when is_binary(other) do
+    {:unknown,other}
   end
 
+  def print({:file_description, items}) do
+    fields =
+      field_list(:file_description)
+      |> Enum.map(fn
+
+        {f, :future, length} -> Keyword.get(items,f) |> String.pad_trailing(length)
+        {f, :date, 8} ->
+          d = Keyword.get(items,f)
+          month = Integer.to_string(d.month) |> String.pad_leading(2,"0")
+          day = Integer.to_string(d.day) |> String.pad_leading(2,"0")
+          year = Integer.to_string(d.year)
+          month <> day <> year
+        {f,_, length} -> Keyword.get(items,f) |> String.pad_trailing(length)
+
+      end) |> Enum.join
+
+    "A0" <> fields <> "\n"
+  end
+
+  def print({:unknown, line}) do
+    line
+  end
+
+  defp field_list(:file_description) do
+    [
+      {:org, :code, 1},
+      {:sdif_version, :alpha, 8},
+      {:file_code, :code, 2},
+      {:fu1, :future, 30},
+      {:software_name, :alpha, 20},
+      {:software_version, :alpha, 10},
+      {:contact_name, :alpha, 20},
+      {:contact_phone, :phone, 12},
+      {:file_date, :date, 8},
+      {:fu2, :future, 42},
+      {:submitted_by_lsc, :alpha, 2},
+      {:fu3,:future, 3}
+    ]
+
+  end
   defp parse_date(str) do
 
     <<
--- test/sdif_test.exs
+++ test/sdif_test.exs
@@ -8,9 +8,20 @@ defmodule SdifTest do
 A01V3      01Meet Entries                  TeamUnify, LLC      2.0       Coach, Some         3191112222  05282019
 
 """
-    # assert Sdif.parse(sample) == {:file_description, [org: "1", sdif_version: "V3", file_code: "01", software_name: "TeamUnify, LLC", software_version: "2.0", contact_name: "Coach, Some", contact_phone: "3191112222", file_date: ~D[2019-05-28]]}
+    assert Sdif.parse(sample) == {:file_description, [org: "1", sdif_version: "V3", file_code: "01", fu1: "Meet Entries", software_name: "TeamUnify, LLC", software_version: "2.0", contact_name: "Coach, Some", contact_phone: "3191112222", file_date: ~D[2019-05-28]]}
 
-    roundtrip =  Sdif.parse(sample) |> Sdif.print
-    assert roundtrip == sample
+    sample2 = sample_line(1)
+    roundtrip =  Sdif.parse(sample2) |> Sdif.print
+    assert roundtrip == sample2
+  end
+
+  def sample_line(num) do
+    stream = File.stream!("test/sample.sd3")
+    [line] = 
+      stream
+      |> Enum.drop(num-1)
+      |> Enum.take(1)
+
+    line
   end
 end
